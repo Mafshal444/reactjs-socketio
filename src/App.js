94d93f13e55b1b612831a6541fed6530b9c90f00
Mafshal444 <mafshal53@gmail.com>
2023-11-23 16:48:27 +0500

    child render issue resolved using useCallback.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Example from "./example";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 function App() {
   const [press, setPress] = useState(0)
   console.log("App Component is Rendered.")
+  const callBack = useCallback(() => {
+    console.log("CallBack To Passed To child.")
+  }, [])
   return (
     <div className="App">
       <input disabled value={press} />
       <button onClick={() => setPress(press+1)}> PressMe </button>
-      <Example />
+      <Example callBack={callBack} />
     </div>
   );
 }
--- src/example.js
+++ src/example.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Example() {
+function Example({ callBack }) {
 	console.log("Example Component is Rendered.")
   return (
     <div className="App">
       <h1>Example Component</h1>
+			<button onClick={callBack}> Press CallBack </button>
     </div>
   );
 }
