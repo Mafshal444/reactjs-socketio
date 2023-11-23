03a05fd35fe90c61c1a80891e86176f376d8c5d9
Mafshal444 <mafshal53@gmail.com>
2023-11-23 16:38:55 +0500

    check first render issue in child component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Example from "./example";
+import { useState } from "react";
 function App() {
+  const [press, setPress] = useState(0)
+  console.log("App Component is Rendered.")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input disabled value={press} />
+      <button onClick={() => setPress(press+1)}> PressMe </button>
+      <Example />
     </div>
   );
 }
---
+++ src/example.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Example() {
+	console.log("Example Component is Rendered.")
+  return (
+    <div className="App">
+      <h1>Example Component</h1>
+    </div>
+  );
+}
+
+export default Example;
