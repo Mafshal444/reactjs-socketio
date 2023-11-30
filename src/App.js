78ecd8b90ec70bc8e786b193b0b5df82114f838d
Mafshal444 <mafshal53@gmail.com>
2023-11-30 12:47:51 +0500

    implemented socketio.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import Example from "./example";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
+// import { socket } from './socket';
+import socketIO from 'socket.io-client';
+
 function App() {
+  // const socket1 = new WebSocket(`wss://trucknow.paklogics.com/socket.io/tracking`)
+  // console.log(socket1)
+  const socket = socketIO.connect(`wss://trucknow.paklogics.com?userId=afshalhasbeenconnected`,{path: "/socket.io/tracking"});
   const [press, setPress] = useState(0)
   console.log("App Component is Rendered.")
   const callBack = useCallback(() => {
     console.log("CallBack To Passed To child.")
   }, [])
+  /*****socket implementation */
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  useEffect(() => {
+    function onConnect() {
+      console.log("socket connected.....")
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    // function onFooEvent(value) {
+    //   setFooEvents(previous => [...previous, value]);
+    // }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    // socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      // socket.off('foo', onFooEvent);
+    };
+  }, []);
+
+  useEffect(()=> {
+    // socket.connect();
+  }, [])
+  console.log(isConnected)
+  /***socket implementaion */
+
   return (
     <div className="App">
       <input disabled value={press} />
